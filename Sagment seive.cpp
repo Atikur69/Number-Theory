4eff41bddb1439349879ce2fe6c21668ecc3f7cb
Atikur69 <66937538+Atikur69@users.noreply.github.com>
2021-08-28 16:17:48 +0600

    Add files via upload

---
+++ Sagment seive.cpp
@@ -0,0 +1,76 @@
+/**
+    Author : Md. Atikur Rahman
+    Node : first see input data and than
+    using long long
+*/
+#include<bits/stdc++.h>
+
+#define fread freopen("in.txt","r",stdin)
+#define fwrite freopen("out.txt","w",stdout)
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define test scanf("%d",&tt)
+#define loop for(int t = 1 ; t<=tt ; t++)
+#define ll long long
+#define MAX 200005
+#define limit 10000000
+#define MOD 1000000007
+#define PI acos(-1)
+#define pii pair<int , int>
+
+using namespace std;
+
+bool primes[limit+1];
+vector<int>vec_prime;
+/// limit er value 10^8 dewa ase problem hisebe komaite hobe
+/// otherwise time limit khaba
+/// 5761455 ato gulo prime store ase
+void seive()
+{
+    memset(primes , true , sizeof primes);
+    primes[0] = primes[1] = false;
+    for(int i = 4 ; i<=limit ; i+=2)primes[i] = false;
+    for(int i = 3 ; i*i<=limit ; i+=2)
+    {
+        if(primes[i] == true)
+        {
+            for(int j = i*i ; j<=limit ; j+=i)
+            {
+                primes[j] = false;
+            }
+        }
+    }
+    vec_prime.push_back(2);
+    for(int i = 3 ; i<=limit ; i+=2)
+    {
+        if(primes[i])vec_prime.push_back(i);
+    }
+}
+
+int main()
+{
+    seive();
+    int tt;
+    test;
+    loop
+    {
+        int m , n;
+        scanf("%d %d",&m , &n);
+        /// sagment seive part ///
+        for(int i = m ; i<=n ; i++)
+        {
+            bool flag = true;
+            for(int j = 0 ; vec_prime[j]*vec_prime[j]<=i ; j++)
+            {
+                if(i%vec_prime[j] == 0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag && i != 1)printf("%d\n",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
